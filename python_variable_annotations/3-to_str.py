45a62916992f5bff7ec80103509faf03510a0aa4
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-19 15:03:01 -0400

    Add files via upload

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Description:
+Function that takes a float (n)
+as argument and returns the string
+representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns n in a string
+    representation.
+    """
+
+    return str(n)

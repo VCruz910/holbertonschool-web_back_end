485aacf2f2a22d5008df1505d2729a7c585f7cae
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-19 14:44:40 -0400

    Add files via upload
    Created function that concatenates two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Description:
+Function that takes
+a string (str1) and a
+string (str2) as arguments
+and returns a concatenated
+string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two strings and
+    returns them as a
+    concatenated string.
+    """
+
+    return str1 + str2

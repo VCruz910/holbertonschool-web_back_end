e732e7666f44e17cc6fe9540d3ebc911f2bd5013
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-19 15:53:11 -0400

    Add files via upload

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Description:
+A function that takes a float
+(multiplier) as argument and
+returns a function that
+multiplies a float by
+multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that
+    multiplies a float by
+    multiplier.
+    """
+
+    return lambda x: x * multiplier

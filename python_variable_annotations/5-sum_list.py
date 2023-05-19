a432f3156205f8001b497de4046c381f1e3d4bd6
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-19 15:21:59 -0400

    Add files via upload

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Description:
+A function that takes a
+list (input_list) of floats
+as argument and returns their
+sum as a float.
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum of the
+    arguments as a float.
+    """
+
+    return sum(input_list)

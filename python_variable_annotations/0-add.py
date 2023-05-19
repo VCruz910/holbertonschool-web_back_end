ccf7362b594f5d499d35cd732aba93d55df42381
VCruz910 <vscruz910@gmail.com>
2023-05-19 14:27:42 -0400

    Created Main files, Readmes, and 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Description:
+Function that takes a float 
+(a) and a float (b) as
+arguments and returns their
+sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of float
+    a and b.
+    """
+
+    return a + b

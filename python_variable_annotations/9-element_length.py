a26b7eda02ccd74f709d270fc373ec971799804b
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-22 08:54:50 -0400

    Add files via upload

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Description:
+A function that returns values
+with the appropiate types.
+"""
+
+from typing import Iterable, Tuple, Sequence, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[List, int]]:
+    """
+    Returns value with
+    appropiate types.
+    """
+    return [(i, len(i)) for i in lst]

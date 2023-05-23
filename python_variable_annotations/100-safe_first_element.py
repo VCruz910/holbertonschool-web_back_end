1aa958f04b7ab34732dbbcc4cbb4294d05eb96e2
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-23 08:42:42 -0400

    Add files via upload

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Description:
+
+"""
+
+from typing import Union, Any, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None

dff9713d902a75339f962201c24e0d2bb60398a9
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-24 08:50:32 -0400

    Add files via upload
    fixed variable errors in code.

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -7,11 +7,11 @@ key in a dictionary.
 
 from typing import TypeVar, Union, Mapping, Any
 
-TV = TypeVar
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[TV, None] = None) -> Union[Any, TV]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     if key in dct:
         return dct[key]
     else:

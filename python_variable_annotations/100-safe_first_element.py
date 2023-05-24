c256a574e3b02cf3c753aab5fd3a36d1013e2fc0
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-05-24 09:00:52 -0400

    Add files via upload
    Added documentation.

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """
 Description:
-
+A function that checks
+the first element safely.
 """
 
 from typing import Union, Any, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Checks elements safely.
+    """
     if lst:
         return lst[0]
     else:

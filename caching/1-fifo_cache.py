7c771a7359df5910ddd6c8d14cac27f73a55ceb9
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-06-09 02:11:52 -0400

    Add files via upload

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Create a class FIFOCache that
+inherits from BaseCaching and
+is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    A class that inherits from 'BaseCaching'
+    and also is a caching system.
+
+    Attributes:
+        __init__ - method that initializes class instance
+        put - method that adds a key/value pair to cache
+        get - method that retrieves a key/value pair from cache
+    """
+
+    def __init__(self):
+        """
+        Initialize class instance.
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Add key/value pair to cache data.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+        """
+        Return value stored in `key` key of cache.
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None

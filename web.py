22829494fd17da44a29761c9f6c6fad1c3f13843
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-06-29 02:31:50 -0400

    Add files via upload

---
+++ web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Expiring Web Cache Module
+"""
+
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+redis = redis.Redis()
+
+
+def wrap_requests(method: Callable) -> Callable:
+    """
+    Decorator
+    Wrapper
+    """
+
+    @wraps(method)
+    def wrapper(url):
+        """
+        Wrapper
+        """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = method(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
+def get_page(url: str) -> str:
+    """
+    Gets page
+    self descriptive
+    """
+    response = requests.get(url)
+    return response.text

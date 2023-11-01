9b56b8bdcce8f6ff75e5277bba61438ba395667f
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2023-11-01 16:48:36 -0400

    Add files via upload

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,14 +1,13 @@
--- Creates a table called users
--- with these attributes
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
---  If the table already exists, your script should not fail
---  Your script can be executed on any database
+-- A SQL script that
+-- creates a table "users"
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-    );
+CREATE TABLE
+IF NOT EXISTS 'users' (
+    'id' int NOT NULL AUTO_INCREMENT,
+    'email' VARCHAR
+(255) NOT NULL UNIQUE,
+    'name' VARCHAR
+(255),
+    PRIMARY KEY
+('id')
+);
